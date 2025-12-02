17b694ae810cbeb88266f64d1cbbf92844fa121b
Runguino247 <oskarreyesbarja@gmail.com>
2025-12-01 21:47:52 -0400

    casi completa
    falta settings

--- appavanzada/lib/main.dart
+++ appavanzada/lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:appavanzada/pantallas/pantalla_areas.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Inicializar formatos en español
+  await initializeDateFormatting('es_ES', null);
+
   runApp(const MainApp());
 }
 
