5739adbd27f0600cb1d017f4f1ed2b5e3e8b2ddd
Oscar <oskarreyesbarja@gmail.com>
2025-12-08 18:35:21 -0400

    finisk=h

--- appavanzada/lib/pantallas/pantalla_nueva_area.dart
+++ appavanzada/lib/pantallas/pantalla_nueva_area.dart
@@ -1,3 +1,4 @@
+import 'package:appavanzada/pantallas/pantalla_areas.dart';
 import 'package:flutter/material.dart';
 import '../models/area.dart';
 import '../db/database_helper.dart';
@@ -53,7 +54,9 @@ class _PantallaNuevaAreaState extends State<PantallaNuevaArea> {
 
               if (finalName.isEmpty) {
                 ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('Ingresa un nombre para el área')),
+                  const SnackBar(
+                    content: Text('Ingresa un nombre para el área'),
+                  ),
                 );
                 return;
               }
@@ -101,7 +104,11 @@ class _PantallaNuevaAreaState extends State<PantallaNuevaArea> {
                       ),
                       child: imageUrl != null
                           ? Image.network(imageUrl, fit: BoxFit.cover)
-                          : const Icon(Icons.image, size: 56, color: Colors.black26),
+                          : const Icon(
+                              Icons.image,
+                              size: 56,
+                              color: Colors.black26,
+                            ),
                     ),
                   ),
                 ],
@@ -167,18 +174,26 @@ class _PantallaNuevaAreaState extends State<PantallaNuevaArea> {
 
   String? _imageForArea(String name) {
     final n = name.toLowerCase();
-    if (n.contains('comedor')) return 'https://planner5d.com/blog/content/images/2025/04/comedor.negro.moderno.jpg';
-    if (n.contains('sala')) return 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIMSPRKFfVsCtWkgx8mNgJvr4FVN6JIeZFJQ&s';
-    if (n.contains('cocina')) return 'https://st.hzcdn.com/simgs/65419c56074741bf_14-8315/home-design.jpg';
-    if (n.contains('dormitorio')) return 'https://content.elmueble.com/medio/2025/04/01/dormitorio-con-cabecero-de-obra-00560141_2bbe7015_00560141_250401120859_2000x1333.webp';
-    if (n.contains('jard')) return 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRA5C8z0O7wLEKqRGhE3D5LbWb07hL4Dwxj4A&s';
-    if (n.contains('garaj')) return 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTUTCBzg8sMF-2cTFNzb6U9lj2DkCyzxYkpQ&s';
-    if(n.contains('baño')) return 'https://inspirame.corona.co/wp-content/uploads/2023/08/productos-lanzamiento-Corona-3-1024x614.jpg';
-    if(n.contains('terraza')) return 'https://st.hzcdn.com/simgs/54d10a2e05cfcd6d_14-7361/home-design.jpg';
-    if(n.contains('patio')) return 'https://st.hzcdn.com/simgs/dec122f30f1444cb_4-2239/contemporaneo-patio.jpg';
+    if (n.contains('comedor'))
+      return 'https://planner5d.com/blog/content/images/2025/04/comedor.negro.moderno.jpg';
+    if (n.contains('sala'))
+      return 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIMSPRKFfVsCtWkgx8mNgJvr4FVN6JIeZFJQ&s';
+    if (n.contains('cocina'))
+      return 'https://st.hzcdn.com/simgs/65419c56074741bf_14-8315/home-design.jpg';
+    if (n.contains('dormitorio'))
+      return 'https://content.elmueble.com/medio/2025/04/01/dormitorio-con-cabecero-de-obra-00560141_2bbe7015_00560141_250401120859_2000x1333.webp';
+    if (n.contains('jard'))
+      return 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRA5C8z0O7wLEKqRGhE3D5LbWb07hL4Dwxj4A&s';
+    if (n.contains('garaj'))
+      return 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTUTCBzg8sMF-2cTFNzb6U9lj2DkCyzxYkpQ&s';
+    if (n.contains('baño'))
+      return 'https://inspirame.corona.co/wp-content/uploads/2023/08/productos-lanzamiento-Corona-3-1024x614.jpg';
+    if (n.contains('terraza'))
+      return 'https://st.hzcdn.com/simgs/54d10a2e05cfcd6d_14-7361/home-design.jpg';
+    if (n.contains('patio'))
+      return 'https://st.hzcdn.com/simgs/dec122f30f1444cb_4-2239/contemporaneo-patio.jpg';
     if (n.isEmpty) return null;
     return null;
-    
   }
 
   String _seedForName(String name) {
@@ -205,6 +220,11 @@ class _PantallaNuevaAreaState extends State<PantallaNuevaArea> {
 
     if (!mounted) return;
 
-    Navigator.of(context).pop(); // volver 1 pantalla
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => PantallaAreas(),
+      ),
+    ); // volver 1 pantalla
   }
 }
